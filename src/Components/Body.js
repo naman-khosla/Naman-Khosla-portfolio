3f8cb31fed9cebfce41db3c5d4ae20389367d99e
Namank98 <100039918+Namank98@users.noreply.github.com>
2024-04-24 00:43:20 +1000

    v1

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,19 @@
+import "./Body.css";
+
 export default function Body() {
   return (
     <div className="body">
-      <img src="images/ProfilePic.png" className="profilePic" />
+      <img src="images/body/profile_picture.png" className="profile_picture" />
       <div>
-        <img src="images/name1.png" className="nameLogo" />
+        <img src="images/body/name_logo.svg" className="name_logo" />
       </div>
+      <h2 className="about_me">
+        With a knack for innovative thinking, I excel in turning abstract ideas
+        into practical business solutions. I thrive in team environments, using
+        motivational techniques to foster a space where ideas flow freely. My
+        relentless drive to lead and innovate keeps me ahead of the curve,
+        setting trends rather than following them.
+      </h2>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer1">
-      <h3>@N11507721 Naman Khosla IFN666</h3>
+      <h3>@N11507721 Naman Khosla IFN666 </h3>
     </div>
   );
 }
