d8bf9196690e784f803c214312a7f1d5fca5efa4
Namank98 <100039918+Namank98@users.noreply.github.com>
2024-04-25 20:05:06 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import "./App.css"; // Basic CSS for styling
 import Navbar from "./Components/Navbar.js";
 import Footer from "./Components/Footer.js";
-import Body from "./Components/Body.js";
-import Introduction from "./Components/Introduction.js";
 import Introduction1 from "./Components/Introduction_1.js";
 import ProfessionalSnap from "./Components/ProfessionalSnap.js";
 
