2ea7b01bc85811e96fd38bb1b820f891cb72efed
Namank98 <100039918+Namank98@users.noreply.github.com>
2024-04-25 19:55:49 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css"; // Basic CSS for styling
 import Navbar from "./Components/Navbar.js";
 import Footer from "./Components/Footer.js";
@@ -7,7 +7,6 @@ import Body from "./Components/Body.js";
 import Introduction from "./Components/Introduction.js";
 import Introduction1 from "./Components/Introduction_1.js";
 import ProfessionalSnap from "./Components/ProfessionalSnap.js";
-
 
 function App() {
   return (
