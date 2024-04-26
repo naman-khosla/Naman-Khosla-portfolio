2ea6d7c2edac196d392beb103bc44a83e091f5a6
Namank98 <100039918+Namank98@users.noreply.github.com>
2024-04-26 20:16:25 +1000

    unused vars

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // Co
 import "./App.css";
 import Navbar from "./Components/Main_Components/Navbar";
 import Footer from "./Components/Main_Components/Footer";
-import Introduction1 from "./Components/Homepage_Components/Introduction_1.js";
-import ProfessionalSnap from "./Components/Homepage_Components/ProfessionalSnap.js";
 import Home from "./Pages/Homepage";
 import About from "./Pages/About";
 import Resume from "./Pages/Resume";
-import Portfolio from "./Pages/Portfolio";
 import Hobbies from "./Pages/Hobbies";
 import PortfolioPage from "./Pages/Pull.js";
 import ProjectDetail from "./Pages/ProjectDetails.js";
--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,5 +1,5 @@
 import Resume_Section_1 from "../Components/Resume_Components/Resume_Section_1.js";
-import Resume_Section_2 from "../Components/Resume_Components/Resume_Section_2.js";
+// import Resume_Section_2 from "../Components/Resume_Components/Resume_Section_2.js";
 import Resume_Section_3 from "../Components/Resume_Components/Resume_Section_3.js";
 
 export default function Resume() {
